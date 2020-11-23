3e34f352b9eb5193838c866a1c004e4f3bdaa6ca
Marc Coury <gloriphobia@gmail.com>
2020-11-23 18:00:12 +0000

    Add IsNetworkError check

--- aws_error.go
+++ aws_error.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/mec07/neterror"
 )
 
 // GetAWSError recursively checks if the error is an awserr.Error. It unwraps
@@ -36,4 +37,26 @@ func GetAWSError(err error) (awserr.Error, bool) {
 	}
 
 	return awsErr, ok
+}
+
+// IsNetworkError checks if a network error has occurred. The reason we can't
+// just use `neterror.GetNetError` is because AWS errors keep the original error
+// in a field instead of wrapping errors, so we have to the original error field
+// too.
+func IsNetworkError(err error) bool {
+	// We may get a non-AWS error
+	_, ok := neterror.GetNetError(err)
+	if ok {
+		return true
+	}
+
+	// Or we may get an awserr.Error, from which we have to extract the
+	// original error
+	awsErr, ok := GetAWSError(err)
+	if !ok {
+		return false
+	}
+
+	_, ok = neterror.GetNetError(awsErr.OrigErr())
+	return ok
 }
--- aws_error_test.go
+++ aws_error_test.go
@@ -3,8 +3,10 @@ package awstokens_test
 import (
 	"errors"
 	"fmt"
+	"net"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"github.com/mec07/awstokens"
 	"github.com/stretchr/testify/assert"
@@ -48,18 +50,70 @@ func TestGetAWSError(t *testing.T) {
 	for _, test := range table {
 		test := test
 		t.Run(test.name, func(t *testing.T) {
-			fmt.Println("before err = ", test.err)
 			awsErr, ok := awstokens.GetAWSError(test.err)
 			if !test.shouldSucceed {
 				assert.False(t, ok)
-				fmt.Println("after err = ", test.err)
 				return
 			}
 			if !ok {
 				t.Fatal("expected this to work")
 			}
 			assert.Equal(t, cognitoidentityprovider.ErrCodeNotAuthorizedException, awsErr.Code())
-			fmt.Println("after err = ", test.err)
+		})
+	}
+}
+
+func TestIsNetworkError(t *testing.T) {
+	origErr := &net.DNSError{}
+	err := awserr.New("code", "message", origErr)
+
+	table := []struct {
+		name          string
+		err           error
+		shouldSucceed bool
+	}{
+		{
+			name:          "aws error on top of network error",
+			err:           err,
+			shouldSucceed: true,
+		},
+		{
+			name:          "wrapped aws error on top of network error",
+			err:           fmt.Errorf("wrap the error: %w", err),
+			shouldSucceed: true,
+		},
+		{
+			name:          "plain network error",
+			err:           origErr,
+			shouldSucceed: true,
+		},
+		{
+			name:          "wrapped network error",
+			err:           fmt.Errorf("another layer: %w", origErr),
+			shouldSucceed: true,
+		},
+		{
+			name:          "nil error",
+			err:           nil,
+			shouldSucceed: false,
+		},
+		{
+			name:          "not a network error",
+			err:           errors.New("not an aws error"),
+			shouldSucceed: false,
+		},
+		{
+			name:          "aws error, but no underlying network error",
+			err:           awserr.New("code", "message", nil),
+			shouldSucceed: false,
+		},
+	}
+
+	for _, test := range table {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			ok := awstokens.IsNetworkError(test.err)
+			assert.Equal(t, test.shouldSucceed, ok)
 		})
 	}
 }
